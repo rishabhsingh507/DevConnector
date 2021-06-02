51fa29b38559c1d9dd633db91a1a440359b5048e
rishabhsingh507 <rishabhsingh507@gmail.com>
2021-06-02 20:36:49 +0530

    feat: connect to database

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('API Running');
 })
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
