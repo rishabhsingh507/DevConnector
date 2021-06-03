bf1b7b211b9301cbeec123e94890f57ba2ede81d
rishabhsingh507 <rishabhsingh507@gmail.com>
2021-06-03 17:05:38 +0530

    feat: Added route for getting github posts info and added post model and routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,47 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Posts route'));
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const Post = require('../../models/Post');
+
+// @route   POST api/posts
+// @desc    Create a post
+// @access Private
+
+router.post(
+    '/',
+    [
+        auth,
+        check('text', 'Text is required')
+            .not()
+            .isEmpty()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            console.log(req.user);
+            const user = await User.findById(req.user.id).select('-password');
+
+            const newPost = new Post({
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            });
+
+            const post = await newPost.save();
+            res.json(post);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+     }
+);
 
 module.exports = router;
