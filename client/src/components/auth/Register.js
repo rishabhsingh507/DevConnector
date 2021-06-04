302a34a7f6b13574636e8757caf3dfa843d203d1
rishabhsingh507 <rishabhsingh507@gmail.com>
2021-06-04 20:27:21 +0530

    feat: Added user login from frontend

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,12 +1,12 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
 import { register } from '../../actions/auth';
 import propTypes from 'prop-types';
 
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -28,6 +28,10 @@ const Register = ({ setAlert, register }) => {
         }
     };
     
+
+    if (isAuthenticated) { 
+        return <Redirect to='/dashboard' />;
+    }
     return (
         <Fragment>
             <h1 className="large text-primary">Sign Up</h1>
@@ -88,6 +92,11 @@ const Register = ({ setAlert, register }) => {
 Register.propTypes = {
     setAlert: propTypes.func.isRequired,
     register: propTypes.func.isRequired,
+    isAuthenticated: propTypes.bool,
 };
 
-export default connect(null, { setAlert, register })(Register);
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
