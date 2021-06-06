39e5c2790abfcb400d9c7ac9a318f96f1a15acb1
rishabhsingh507 <rishabhsingh507@gmail.com>
2021-06-06 19:22:15 +0530

    feat: Added like and unlike functionality from posts

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,19 +3,25 @@ import propTypes from 'prop-types'
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { addLike, removeLike } from '../../actions/post'
 
-const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date } }) => {
+const PostItem = ({
+    addLike,
+    removeLike,
+    auth,
+    post: { _id, text, name, avatar, user, likes, comments, date }
+}) => {
     return (
         <div class="post bg-white p-1 my-1">
             <div>
-                <a href="profile.html">
+                <Link to={`/profile/${user}`}>
                     <img
                         class="round-img"
                         src={avatar}
                         alt=""
                     />
                     <h4>{name}</h4>
-                </a>
+                </Link>
             </div>
             <div>
                 <p class="my-1">
@@ -24,14 +30,14 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
                 <p class="post-date">
                     Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
                 </p>
-                <button type="button" class="btn btn-light">
-                    <i class="fas fa-thumbs-up" />{' '}
+                <button onClick={() => addLike(_id)} type="button" class="btn btn-light">
+                    <i className="fas fa-thumbs-up" />{' '}
                     <span>{likes.length > 0 && (
                         <span>{likes.length}</span>
                     )}</span>
                 </button>
-                <button type="button" class="btn btn-light">
-                    <i class="fas fa-thumbs-down"></i>
+                <button onClick={() => removeLike(_id)} type="button" class="btn btn-light">
+                    <i className="fas fa-thumbs-down"></i>
                 </button>
                 <Link to={`/post/${_id}`} class="btn btn-primary">
                     Discussion{' '}
@@ -58,4 +64,4 @@ const mapStateToProps = state => ({
     auth: state.auth
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
